9251516520a48479f28da915cc3d8abe750fe700
Komesh Bathula <komeshbathula@gmail.com>
2026-01-23 20:39:02 +0530

    Add Soldier and Bananas [A] from Codeforces

---
+++ 546/A - Soldier and Bananas/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int k = sc.nextInt();    int n = sc.nextInt();    int w = sc.nextInt();    int totalCost = 0;    for (int i = 1; i <= w; i++) {      totalCost += i * k;    }    System.out.println(totalCost > n ? totalCost - n : 0);    sc.close();  }}
