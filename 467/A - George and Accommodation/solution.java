687982bc30323c6162c8f8fba540aed57b32e27d
Komesh Bathula <komeshbathula@gmail.com>
2026-01-28 14:21:11 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int n = sc.nextInt();    int count_of_rooms = 0;    for (int i = 0; i < n; i++) {      int p = sc.nextInt();      int q = sc.nextInt();      if (q - p >= 2) {        count_of_rooms++;      }    }    System.out.println(count_of_rooms);  }}
