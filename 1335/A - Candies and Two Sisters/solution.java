279ce37b74c161a9681a9f33c416542ec7ba8282
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:19:19 +0530

    Add Candies and Two Sisters [A] from Codeforces

--- 1335/A - Candies and Two Sisters/solution.java
+++ 1335/A - Candies and Two Sisters/solution.java
@@ -1,1 +1,13 @@
-import java.util.Scanner; public class Main {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int t = sc.nextInt();         while (t-- > 0) {            long n = sc.nextLong();   // n can be up to 2e9            System.out.println((n - 1) / 2);        }    }}
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long n = sc.nextLong();   // n can be up to 2e9
+            System.out.println((n - 1) / 2);
+        }
+    }
+}
