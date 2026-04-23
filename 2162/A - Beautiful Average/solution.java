168ad407f72c92c23b144a9dac3c4965a6378b3d
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:16:46 +0530

    Add Beautiful Average [A] from Codeforces

--- 2162/A - Beautiful Average/solution.java
+++ 2162/A - Beautiful Average/solution.java
@@ -1,1 +1,15 @@
-import java.util.Scanner;public class BeautifulAverage{    public static void main(String [] args){        Scanner sc = new Scanner(System.in);        int tc = sc.nextInt();        while(tc-- > 0){            int n = sc.nextInt();            int arr[] = new int[n];            for(int i = 0;i<n;i++) arr[i] = sc.nextInt();            int max = arr[0];            for(int i : arr) if(i>max) max = i;            System.out.print(max+" ");        }    }}
+import java.util.Scanner;
+public class BeautifulAverage{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc-- > 0){
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i = 0;i<n;i++) arr[i] = sc.nextInt();
+            int max = arr[0];
+            for(int i : arr) if(i>max) max = i;
+            System.out.print(max+" ");
+        }
+    }
+}
