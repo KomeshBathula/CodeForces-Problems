5523b3a1907ce47702801da9f1beea0444c40c96
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:17:08 +0530

    Add Petya and Strings [A] from Codeforces

--- 112/A - Petya and Strings/solution.java
+++ 112/A - Petya and Strings/solution.java
@@ -1,1 +1,22 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    String s1 = sc.nextLine();    String s2 = sc.nextLine();    for (int i = 0; i < s1.length(); i++) {      char ch1 = Character.toLowerCase(s1.charAt(i));      char ch2 = Character.toLowerCase(s2.charAt(i));      if (ch1 < ch2) {        System.out.println(-1);        return;      } else if (ch1 > ch2) {        System.out.println(1);        return;      }    }    System.out.println(0);    sc.close();  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s1 = sc.nextLine();
+    String s2 = sc.nextLine();
+    for (int i = 0; i < s1.length(); i++) {
+      char ch1 = Character.toLowerCase(s1.charAt(i));
+      char ch2 = Character.toLowerCase(s2.charAt(i));
+      if (ch1 < ch2) {
+        System.out.println(-1);
+        return;
+      } else if (ch1 > ch2) {
+        System.out.println(1);
+        return;
+      }
+    }
+    System.out.println(0);
+    sc.close();
+  }
+}
