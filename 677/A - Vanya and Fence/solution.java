7c479cdb1379ae259e6243642f4fedf7bda2f3eb
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:18:05 +0530

    Add Vanya and Fence [A] from Codeforces

--- 677/A - Vanya and Fence/solution.java
+++ 677/A - Vanya and Fence/solution.java
@@ -1,1 +1,24 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int n = sc.nextInt();    int h = sc.nextInt();    int[] arr = new int[n];    for (int i = 0; i < n; i++) {      arr[i] = sc.nextInt();    }    int width = 0;    for (int i = 0; i < n; i++) {      if (arr[i] > h) {        width += 2;       }      else {        width += 1;       }    }    System.out.println(width);      }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int h = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int width = 0;
+    for (int i = 0; i < n; i++) {
+      if (arr[i] > h) {
+        width += 2; 
+      }
+      else {
+        width += 1; 
+      }
+    }
+    System.out.println(width);
+    
+  }
+}
