f723c3d92c1b4329b302278c56f85ba65d9ca5b3
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:18:09 +0530

    Add Beautiful Year [A] from Codeforces

--- 271/A - Beautiful Year/solution.java
+++ 271/A - Beautiful Year/solution.java
@@ -1,1 +1,20 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int year = sc.nextInt();    year++;    while (true) {      String y = Integer.toString(year);      if (y.charAt(0) != y.charAt(1) && y.charAt(0) != y.charAt(2) && y.charAt(0) != y.charAt(3) &&          y.charAt(1) != y.charAt(2) && y.charAt(1) != y.charAt(3) &&          y.charAt(2) != y.charAt(3)) {        System.out.println(year);        break;      }      year++;    }    sc.close();  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+    year++;
+    while (true) {
+      String y = Integer.toString(year);
+      if (y.charAt(0) != y.charAt(1) && y.charAt(0) != y.charAt(2) && y.charAt(0) != y.charAt(3) &&
+          y.charAt(1) != y.charAt(2) && y.charAt(1) != y.charAt(3) &&
+          y.charAt(2) != y.charAt(3)) {
+        System.out.println(year);
+        break;
+      }
+      year++;
+    }
+    sc.close();
+  }
+}
