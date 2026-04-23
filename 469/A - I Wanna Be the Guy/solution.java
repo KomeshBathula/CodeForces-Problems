29b4b7bd330c1ac2bae57a229044062b5a63212d
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:19:09 +0530

    Add I Wanna Be the Guy [A] from Codeforces

--- 469/A - I Wanna Be the Guy/solution.java
+++ 469/A - I Wanna Be the Guy/solution.java
@@ -1,1 +1,26 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int n = sc.nextInt();    int p1 = sc.nextInt();    boolean[] levels = new boolean[n + 1];    for (int i = 0; i < p1; i++) {      int level = sc.nextInt();      levels[level] = true;    }    int p2 = sc.nextInt();    for (int i = 0; i < p2; i++) {      int level = sc.nextInt();      levels[level] = true;     }    for (int i = 1; i <= n; i++) {      if (!levels[i]) {        System.out.println("Oh, my keyboard!");        return;      }    }    System.out.println("I become the guy.");  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int p1 = sc.nextInt();
+    boolean[] levels = new boolean[n + 1];
+    for (int i = 0; i < p1; i++) {
+      int level = sc.nextInt();
+      levels[level] = true;
+    }
+    int p2 = sc.nextInt();
+    for (int i = 0; i < p2; i++) {
+      int level = sc.nextInt();
+      levels[level] = true; 
+    }
+    for (int i = 1; i <= n; i++) {
+      if (!levels[i]) {
+        System.out.println("Oh, my keyboard!");
+        return;
+      }
+    }
+    System.out.println("I become the guy.");
+  }
+}
