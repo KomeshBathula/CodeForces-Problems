a18d1d8465aa3b520bedfcd608a5805ef9e787c4
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:18:13 +0530

    Add Tram [A] from Codeforces

--- 116/A - Tram/solution.java
+++ 116/A - Tram/solution.java
@@ -1,1 +1,19 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int n = sc.nextInt();    int currentPassengers = 0;    int maxPassengers = 0;    for (int i = 0; i < n; i++) {      int exit = sc.nextInt();      int enter = sc.nextInt();      currentPassengers = currentPassengers - exit + enter;      if (currentPassengers > maxPassengers) {        maxPassengers = currentPassengers;      }    }    System.out.println(maxPassengers);  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int currentPassengers = 0;
+    int maxPassengers = 0;
+    for (int i = 0; i < n; i++) {
+      int exit = sc.nextInt();
+      int enter = sc.nextInt();
+      currentPassengers = currentPassengers - exit + enter;
+      if (currentPassengers > maxPassengers) {
+        maxPassengers = currentPassengers;
+      }
+    }
+    System.out.println(maxPassengers);
+  }
+}
