951a5dde305cee89f4d23d52474781861079cb87
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:17:16 +0530

    Add Helpful Maths [A] from Codeforces

--- 339/A - Helpful Maths/solution.java
+++ 339/A - Helpful Maths/solution.java
@@ -1,1 +1,26 @@
-import java.util.ArrayList;import java.util.Collections;import java.util.Scanner; public class HelpfulMaths {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    String str = sc.nextLine();    ArrayList<Integer> numbers = new ArrayList<>();    for (int i = 0; i < str.length(); i++) {      if (Character.isDigit(str.charAt(i))) {        numbers.add(Character.getNumericValue(str.charAt(i)));      }     }    Collections.sort(numbers);    // System.out.println(numbers);    int idx = 0;    StringBuilder sb = new StringBuilder();    for (int i = 0; i < numbers.size(); i++) {      sb.append(numbers.get(idx++));      if (i != numbers.size() - 1) sb.append('+');    }    System.out.println(sb);    sc.close();  }}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+ 
+public class HelpfulMaths {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    ArrayList<Integer> numbers = new ArrayList<>();
+    for (int i = 0; i < str.length(); i++) {
+      if (Character.isDigit(str.charAt(i))) {
+        numbers.add(Character.getNumericValue(str.charAt(i)));
+      } 
+    }
+    Collections.sort(numbers);
+    // System.out.println(numbers);
+    int idx = 0;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < numbers.size(); i++) {
+      sb.append(numbers.get(idx++));
+      if (i != numbers.size() - 1) sb.append('+');
+    }
+    System.out.println(sb);
+    sc.close();
+  }
+}
