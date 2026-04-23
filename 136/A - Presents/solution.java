779702df5c62506d7cfb65202fd0b0b566db3cfb
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:18:32 +0530

    Add Presents [A] from Codeforces

--- 136/A - Presents/solution.java
+++ 136/A - Presents/solution.java
@@ -1,1 +1,19 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int n = sc.nextInt();    int[] arr = new int[n];    for (int i = 0; i < n; i++) {      arr[i] = sc.nextInt();    }    int[] result = new int[n];    for (int i = 0; i < n; i++) {      result[arr[i] - 1] = i + 1;    }    for (int i = 0; i < n; i++) {      System.out.print(result[i] + " ");    }  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++) {
+      result[arr[i] - 1] = i + 1;
+    }
+    for (int i = 0; i < n; i++) {
+      System.out.print(result[i] + " ");
+    }
+  }
+}
