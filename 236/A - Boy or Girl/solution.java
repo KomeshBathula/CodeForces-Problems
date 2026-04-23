dfe59cee86ea36cf51a0bbb3baaa2f115cd065af
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:17:12 +0530

    Add Boy or Girl [A] from Codeforces

--- 236/A - Boy or Girl/solution.java
+++ 236/A - Boy or Girl/solution.java
@@ -1,1 +1,19 @@
-import java.util.HashSet;import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    String username = sc.nextLine();    HashSet<Character> chars = new HashSet<>();    for (int i = 0; i < username.length(); i++) {      chars.add(username.charAt(i));    }    if (chars.size() % 2 == 0) {      System.out.println("CHAT WITH HER!");    } else {      System.out.println("IGNORE HIM!");    }    sc.close();  }}
+import java.util.HashSet;
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String username = sc.nextLine();
+    HashSet<Character> chars = new HashSet<>();
+    for (int i = 0; i < username.length(); i++) {
+      chars.add(username.charAt(i));
+    }
+    if (chars.size() % 2 == 0) {
+      System.out.println("CHAT WITH HER!");
+    } else {
+      System.out.println("IGNORE HIM!");
+    }
+    sc.close();
+  }
+}
