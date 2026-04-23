21bed9588045cf09cda0f700d1f1372fc0c2431a
Komesh Bathula <komeshbathula@gmail.com>
2026-04-23 13:19:46 +0530

    Add Divisibility Problem [A] from Codeforces

--- 1328/A - Divisibility Problem/solution.java
+++ 1328/A - Divisibility Problem/solution.java
@@ -1,1 +1,14 @@
-import java.util.Scanner; public class Main {  public static void main(String[] args) {    Scanner sc = new Scanner(System.in);    int tc = sc.nextInt();    while (tc -- > 0) {      int a = sc.nextInt();      int b = sc.nextInt();      if (a % b == 0 ) System.out.println(0);      else System.out.println(b - (a % b) );    }  }}
+import java.util.Scanner;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int tc = sc.nextInt();
+    while (tc -- > 0) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      if (a % b == 0 ) System.out.println(0);
+      else System.out.println(b - (a % b) );
+    }
+  }
+}
