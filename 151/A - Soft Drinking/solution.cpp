f08d5642e7d527481be7ea436c3ab8ed04b9f257
Komesh Bathula <komeshbathula@gmail.com>
2026-04-27 19:46:07 +0530

    Add Soft Drinking [A] from Codeforces

---
+++ 151/A - Soft Drinking/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+    int n, k, l, c, d, p, nl, np;
+ 
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+ 
+    int kl = k * l;
+    int toasts = kl / nl;
+ 
+    int limes = c * d;
+    int salt = p / np;
+ 
+    cout << min(toasts, min(limes, salt)) / n;
+ 
+    return 0;
+}
